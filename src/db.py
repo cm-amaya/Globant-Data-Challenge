1d4eadf9f9f7e621bf63baf0b13afbc3a9348fcf
Cristian M. Amaya <cristian.amaya@factored.ai>
2026-01-31 18:15:41 -0500

    Fix init_db function
    Checks that the employee table exists

--- src/db.py
+++ src/db.py
@@ -1,7 +1,7 @@
-import sqlalchemy as sa
-from sqlmodel import SQLModel
-from sqlmodel import Session, create_engine, select
-from .config import settings
+from sqlalchemy import inspect
+from sqlmodel import SQLModel, Session, create_engine, select
+from config import settings
+from models import Employee
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -11,9 +11,6 @@ def init_db(session: Session) -> None:
     Args:
         session (Session): Database session
     """
-    alchemy_engine = sa.create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
-    insp = sa.inspect(alchemy_engine)
-    if insp.has_table("employee", schema=settings.POSTGRES_DB):
-        return
-    else:
+    inspector = inspect(engine)
+    if not inspector.has_table('employee'):
         SQLModel.metadata.create_all(engine)
