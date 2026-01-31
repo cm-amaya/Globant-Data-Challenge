e5cae438e0f03c60bf7f43e44fe3c612ba949d33
Cristian M. Amaya <cristian.amaya@factored.ai>
2026-01-31 18:56:37 -0500

    Add test endpoint

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
-from sqlmodel import Session
+from typing import Annotated
+
+from fastapi import FastAPI, Depends, Query
+from sqlmodel import Session, select
 from db import engine, init_db
 from config import settings
+from models import Employee
 
 app = FastAPI(title=settings.app_name)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 @app.on_event("startup")
 def on_startup():
@@ -13,3 +22,12 @@ def on_startup():
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/employees/")
+def get_employees(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Employee]:
+    employees = session.exec(select(Employee).offset(offset).limit(limit)).all()
+    return employees
