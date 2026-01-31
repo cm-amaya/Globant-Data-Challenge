ddcb1cef08beff02c9a2d6b704177084ca2bfb30
Cristian M. Amaya <cristian.amaya@factored.ai>
2026-01-31 17:48:41 -0500

    Create models.py
    Initial models for the database tables

---
+++ src/models.py
@@ -0,0 +1,16 @@
+from sqlmodel import Field, Relationship, SQLModel
+
+class Department(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    department: str = Field(default=None)
+
+class Job(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    job: str = Field(default=None)
+
+class Employee(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(default=None)
+    datetime: str = Field(default=None)
+    department_id: str = Field(foreign_key="department.id")
+    job_id: str = Field(foreign_key="job.id")
