5b7a893f3f1bb7a6bfdca07a20e9fc383fa26c38
Cristian M. Amaya <cristian.amaya@factored.ai>
2026-01-31 19:00:22 -0500

    Update employee to take null values

--- src/models.py
+++ src/models.py
@@ -10,7 +10,7 @@ class Job(SQLModel, table=True):
 
 class Employee(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    name: str = Field(default=None)
-    datetime: str = Field(default=None)
-    department_id: int = Field(foreign_key="department.id")
-    job_id: int = Field(foreign_key="job.id")
+    name: str | None  = Field(default=None)
+    datetime: str | None  = Field(default=None)
+    department_id: int | None  = Field(foreign_key="department.id")
+    job_id: int | None = Field(foreign_key="job.id")
